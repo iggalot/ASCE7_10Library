ad33751fc7748b266f8c31e24c837b1ade558e8b
Jim <iggalot.iggy@gmail.com>
2021-09-30 09:27:52 -0500

    Wall pressures now computed.
    Changed diagram to show pressure, P in stead of dynamic pressure Q.
    Diagram now shows WW and LW pressures

---
+++ BuildingInfo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ASCE7_10Library
+{
+    /// <summary>
+    /// Class to hold basic building information
+    /// </summary>
+    public class BuildingInfo
+    {
+        public double B { get; set; }    // length normal to wind
+        public double L { get; set; }    // length parallel to wind
+        public double H { get; set; }    // mean roof height
+
+        RiskCategories RiskCat = RiskCategories.II;
+
+        double theta = 25; // roof slope
+
+        public double GetL_over_B()
+        {
+            return L / B;
+        }
+
+        public double GetH_over_L()
+        {
+            return H / L;
+        }
+
+        public BuildingInfo(double b, double l, double h, double angle, RiskCategories cat = RiskCategories.II)
+        {
+            string status_msg = "";
+            bool validInput = true;
+            if (b <= 0)
+            {
+                status_msg += "B cannot be less than zero! " + b.ToString() + "\n";
+                validInput = false;
+            }
+
+            if (l <= 0)
+            {
+                status_msg += "L cannot be less than zero! " + l.ToString() + "\n";
+                validInput = false;
+            }
+
+            if (h <= 0)
+            {
+                status_msg += "H cannot be less than zero! " + h.ToString() + "\n";
+                validInput = false;
+            }
+
+            if (!validInput)
+                throw new InvalidOperationException(status_msg);
+
+
+            // Otherwise create the object
+            B = b;
+            L = l;
+            H = h;
+            theta = angle;
+        }
+    }
+}
---
+++ Constants.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCE7_10Library
+{
+    /// <summary>
+    /// Enum for the ASCE7-10 Exposure Categories
+    /// </summary>
+    public enum ExposureCategories
+    {
+        B = 0,
+        C = 1,
+        D = 2
+    }
+
+    /// <summary>
+    /// Enum for the ASCE7-10 Risk Categories
+    /// </summary>
+    public enum RiskCategories
+    {
+        I = 0,
+        II = 1,
+        III = 2,
+        IV = 3
+    }
+
+    public static class Constants
+    {
+
+    }
+}
