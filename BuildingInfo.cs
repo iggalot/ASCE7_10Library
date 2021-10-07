dff755c112c5237c2e2b4d404f210ff8262c18cd
Jim <iggalot.iggy@gmail.com>
2021-10-07 16:38:24 -0500

    Changed Building Info into Vector4 format.
    -- Generalized the RoofZonePoints for flat roof and sloped roof models
    -- Sloped roof subclass still needs to be further developed.

--- BuildingInfo.cs
+++ BuildingInfo.cs
@@ -14,7 +14,7 @@ namespace ASCE7_10Library
 
         RiskCategories RiskCat = RiskCategories.II;
 
-        public double RoofSlope = 25; // roof slope
+        public double RoofSlope = 0; // default is flat roof
 
         public virtual double[] RoofProfile {get; set;}
 
@@ -32,15 +32,16 @@ namespace ASCE7_10Library
         public Vector4 WW_15_1 { get; set; }
         public Vector4 WW_H_1 { get; set; }
         public Vector4 ORIGIN { get; set; } = new Vector4();
-        public Vector4 RIDGE_1 { get; set; } = new Vector4();
-
         public Vector4 LW_H_1 { get; set; }
         public Vector4 LW_15_1 { get; set; }
         public Vector4 LW_GRD_1 { get; set; }
 
+        // Contains the coordinate for the ridge point
+        public Vector4 RIDGE_1 { get; set; }
 
-        public double[] RoofZonePts { get; set; }
+        public Vector4[] RoofZonePts { get; set; }
 
+        public bool HasSingleRidge { get; set; } = false;
 
         /// <summary>
         /// Flat roof building constructor
@@ -76,11 +77,11 @@ namespace ASCE7_10Library
             if (!validInput)
                 throw new InvalidOperationException(status_msg);
 
-
-
+            // Otherwise create the object
+            B = b;
+            L = l;
 
             // Find the mean height of the roof (the max of 0, 2, 4, ... elements of the roof profile
-
             RiskCat = cat;
 
             // Vectors for the points of the structure profile based on the provided dimensions
@@ -96,94 +97,77 @@ namespace ASCE7_10Library
             // Set the origin of the model to be the midpoint at ground level
             ORIGIN = new Vector4((float)0.5 * (WW_GRD_1.X + LW_GRD_1.X), (float)0.5 * (WW_GRD_1.Y + LW_GRD_1.Y), (float)0.5 * (WW_GRD_1.Z + LW_GRD_1.Z), 1.0f);
 
-            RoofZonePts = GetFlatRoofPressureZonePoints(0, 0);
-
             // The flat roof profile points for the roof
             RoofProfile = roof_profile;
             RoofSlope = Math.Atan((RoofProfile[3]-RoofProfile[1]) / (RoofProfile[2] - RoofProfile[0]));
 
-            // Otherwise create the object
-            B = b;
-            L = l;
-
             // Determine our mean roof height -- using the y-coords (1, 3, 5, 7, ...) of roof_profile
+            // and determine if the roof is flat or not.
             double max_ht = 0;
-            for (int i = 1; i < roof_profile.Length; i=i+2)
+            double wall_ht = roof_profile[1];
+            HasSingleRidge = false;
+            for (int i = 1; i < roof_profile.Length; i = i + 2)
             {
-                if(roof_profile[i] > max_ht)
+                if (roof_profile[1] != roof_profile[i])
+                    HasSingleRidge = true;
+                if (roof_profile[i] > max_ht)
                 {
                     max_ht = roof_profile[i];
                     RIDGE_1 = new Vector4((float)roof_profile[i - 1], (float)roof_profile[i], 0.0f, 1.0f);
                 }
             }
 
-
             // Take the average of the lesser of the windward wall height and the leeward wall height -- needed in case of unequal wall heights.
-            H = 0.5 * (max_ht + Math.Min(roof_profile[1], roof_profile[roof_profile.Length -1]));
+            H = 0.5 * (max_ht + Math.Min(roof_profile[1], roof_profile[roof_profile.Length - 1]));
+
+            // Must be after the H declaration
+            RoofZonePts = GetFlatRoofPressureZonePoints();
+
+
         }
 
         /// <summary>
         /// Get the zone boundary locations for the roof measure relative to a reference point (usually 0,0)
         /// </summary>
-        /// <param name="x_ref">x-coord reference location (usually the top of the windward wall</param>
-        /// <param name="y_ref">y-coord reference location (usually the top of the windward wall</param>
         /// <returns></returns>
-        private double[] GetFlatRoofPressureZonePoints(double x_ref, double y_ref)
-        {
-            // Z1
-            double x_roof_z1_start = x_ref;
-            double x_roof_z2_start = x_ref;
-            double x_roof_z3_start = x_ref;
-            double x_roof_z4_start = x_ref;
-
-            double x_roof_z1_end;
-            double x_roof_z2_end;
-            double x_roof_z3_end;
-            double x_roof_z4_end;
-
+        public virtual Vector4[] GetFlatRoofPressureZonePoints()
+        { 
             if (L < H * 0.5)
             {
-                x_roof_z1_end = x_ref + L;
-                x_roof_z2_start = x_ref + L;
-                x_roof_z2_end = x_ref + L;
-                x_roof_z3_start = x_ref +L;
-                x_roof_z3_end = x_ref + L;
-                x_roof_z4_start = x_ref +L;
-                x_roof_z4_end = x_ref + L;
+                return new Vector4[] {
+                WW_H_1,
+                LW_H_1};
             }
             else if (L < H)
             {
-                x_roof_z1_end = x_ref + H * 0.5;
-                x_roof_z2_start = x_ref + H * 0.5;
-                x_roof_z2_end = x_ref + L;
-                x_roof_z3_start = x_ref + L;
-                x_roof_z3_end = x_ref + L;
-                x_roof_z4_start = x_ref + L;
-                x_roof_z4_end = x_ref + L;
+                return new Vector4[] {
+                WW_H_1,
+                new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, 0.0f, 1.0f),
+                new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, 0.0f, 1.0f),
+                LW_H_1};
             }
             else if (L < H * 2.0)
             {
-                x_roof_z1_end = x_ref + H * 0.5;
-                x_roof_z2_start = x_ref + H * 0.5;
-                x_roof_z2_end = x_ref + H;
-                x_roof_z3_start = x_ref + H;
-                x_roof_z3_end = x_ref + L;
-                x_roof_z4_start = x_ref +L;
-                x_roof_z4_end = x_ref + L;
+                return new Vector4[] {
+                WW_H_1,
+                new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, 0.0f, 1.0f),
+                new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, 0.0f, 1.0f),
+                new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, 0.0f, 1.0f),
+                new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, 0.0f, 1.0f),
+                LW_H_1};
             }
             else
             {
-                x_roof_z1_end = x_ref + H * 0.5;
-                x_roof_z2_start = x_ref + H * 0.5;
-                x_roof_z2_end = x_ref + H;
-                x_roof_z3_start = x_ref + H;
-                x_roof_z3_end = x_ref + H * 2.0;
-                x_roof_z4_start = x_ref + H * 2.0;
-                x_roof_z4_end = x_ref + L;
+                return new Vector4[] {
+                WW_H_1,
+                new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, 0.0f, 1.0f),
+                new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, 0.0f, 1.0f),
+                new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, 0.0f, 1.0f),
+                new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, 0.0f, 1.0f),
+                new Vector4((float)(WW_H_1.X + H * 2.0), WW_H_1.Y, 0.0f, 1.0f),
+                new Vector4((float)(WW_H_1.X + H * 2.0), WW_H_1.Y, 0.0f, 1.0f),
+                LW_H_1};
             }
-
-            return new double[] { x_roof_z1_start, x_roof_z1_end, x_roof_z2_start, x_roof_z2_end, x_roof_z3_start, x_roof_z3_end, x_roof_z4_start, x_roof_z4_end };
         }
-
     }
 }
--- SlopedRoofBuildingInfo.cs
+++ SlopedRoofBuildingInfo.cs
@@ -9,9 +9,6 @@ namespace ASCE7_10Library
 {
     public class SlopedRoofBuildingInfo : BuildingInfo
     {
-        // Contains the coordinate for the ridge point
-        public Vector3 RIDGE_1 { get; set; }
-
         public override double[] RoofProfile {get; set;}
 
         /// <summary>
@@ -25,7 +22,42 @@ namespace ASCE7_10Library
         /// <param name="risk">ASCE7-10 risk category</param>
         public SlopedRoofBuildingInfo(double b, double l, double h, double[] roof_profile, RiskCategories risk) : base(b, l, h, roof_profile, risk)
         {
+            if (roof_profile.Length / 2.0 <= 4)
+                throw new InvalidOperationException("Roof profile for a sloped roof building must contain at least three valid.  Your input has: " + roof_profile.Length / 2.0);
+            if  (roof_profile.Length % 2.0 != 0)
+            {
+                throw new InvalidOperationException("Roof profile must have an even number of elements for x and y coords");
+            }
 
+
+            // Compute the slope from ridge point to WW wall
+            // TODO:: Need a better calculation for this -- gambrel roof?  sawtooth roof?
+            if (HasSingleRidge)
+            {
+                RoofSlope = Math.Atan((RIDGE_1.Y - WW_H_1.Y) / (RIDGE_1.X - WW_H_1.X)) * 180.0 / Math.PI;
+            } else
+            {
+                throw new InvalidOperationException("undefined roof type in calculating RoofSlope -- must be either flat or single ridge for now");
+            }
+
+            RoofZonePts = GetFlatRoofPressureZonePoints();
+        }
+
+        /// <summary>
+        /// Get the zone boundary locations for the roof measure relative to a reference point (usually 0,0)
+        /// </summary>
+        /// <param name="x_ref">x-coord reference location (usually the top of the windward wall</param>
+        /// <param name="y_ref">y-coord reference location (usually the top of the windward wall</param>
+        /// <returns></returns>
+        public override Vector4[] GetFlatRoofPressureZonePoints()
+        {
+            if (HasSingleRidge)
+            {
+                return new Vector4[] { WW_H_1, RIDGE_1, RIDGE_1, LW_H_1 };
+            } else
+            {
+                throw new InvalidOperationException("undefined roof type in calculating RoofSlope -- must be either flat or single ridge for now");
+            }
         }
     }
 }
