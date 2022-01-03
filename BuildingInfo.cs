beb8f15d61db6cee80e620f72b35424d9648c32d
Jim <iggalot.iggy@gmail.com>
2022-01-03 16:22:41 -0600

    Recreating the Building info class object.
    - Sloped roof building types not supported at this time.

--- BuildingInfo.cs
+++ BuildingInfo.cs
@@ -20,9 +20,10 @@ namespace ASCE7_10Library
         public double RoofSlope = 0; // angle of the roof default is flat roof
         public RoofSlopeTypes RoofSlopeType = RoofSlopeTypes.ROOF_SLOPE_FLAT;
 
-        public virtual Vector4[] RoofProfile_1 {get; set;}
-        public virtual Vector4[] RoofProfile_2 { get; set; }
-
+        // Building site parameters
+        public RiskCategories RiskCategory { get; set; } = RiskCategories.II;
+        public ExposureCategories ExposureCategory { get; set; } = ExposureCategories.B;
+        public float WindSpeed { get; set; } = 115.0f;  // wind speed in MPH
 
         public double GetL_over_B()
         {
@@ -33,15 +34,6 @@ namespace ASCE7_10Library
         {
             return H / L;
         }
-        /// <summary>
-        /// Model Axis points
-        /// </summary>
-        public Vector4 UCS_X_START { get; set; }
-        public Vector4 UCS_X_END { get; set; }
-        public Vector4 UCS_Y_START { get; set; }
-        public Vector4 UCS_Y_END { get; set; }
-        public Vector4 UCS_Z_START { get; set; }
-        public Vector4 UCS_Z_END { get; set; }
 
         /// <summary>
         /// Building coords
@@ -81,172 +73,141 @@ namespace ASCE7_10Library
         /// <param name="angle"></param>
         /// <param name="cat"></param>
 
-        public BuildingInfo(Vector4[] roof_profile_1, Vector4[] roof_profile_2, RoofSlopeTypes roof_slope_type, RiskCategories cat = RiskCategories.II, WindOrientations orient=WindOrientations.WIND_ORIENTATION_NORMALTORIDGE)
+        public BuildingInfo(float length, float width, float height, RoofSlopeTypes roof_slope_type = RoofSlopeTypes.ROOF_SLOPE_FLAT, RiskCategories cat = RiskCategories.II, ExposureCategories exp = ExposureCategories.B, float wind_speed = 115.0f)
         {
-            float ucs_axis_length = 300;
-            // UCS points
-            UCS_X_START = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
-            UCS_Y_START = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
-            UCS_Z_START = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
-
-            UCS_X_END = new Vector4(ucs_axis_length, 0.0f, 0.0f, 1.0f);
-            UCS_Y_END = new Vector4(0.0f, ucs_axis_length, 0.0f, 1.0f);
-            UCS_Z_END = new Vector4(0.0f, 0.0f, ucs_axis_length, 1.0f);
-
-
             // Find the mean height of the roof (the max of 0, 2, 4, ... elements of the roof profile
             RiskCat = cat;
             RoofSlopeType = roof_slope_type;
-            // The roof profile points for the roof
-            RoofProfile_1 = roof_profile_1;
-            RoofProfile_2 = roof_profile_2;
 
-            string status_msg = "";
-            int count_1 = roof_profile_1.Length;
-            int count_2 = roof_profile_2.Length;
+            L = length; // parallel to wind
+            B = width;  // perp to wind
+            H = height; // mean roof height
 
-            // TODO:: Fix the B and L calculations here....B non functional and L broken for Parallel direction views
-            if(orient == WindOrientations.WIND_ORIENTATION_NORMALTORIDGE)
-            {
-                B = Math.Abs(roof_profile_2[0].Z - roof_profile_1[0].Z);
-                L = Math.Max(Math.Abs(roof_profile_1[count_1 - 1].X - roof_profile_1[0].X), Math.Abs(roof_profile_2[count_2 - 1].X - roof_profile_2[0].X));
 
-                // Use the East Wall 1 to determine the roof slope factor
-                RoofSlope = Math.Atan((RoofProfile_1[1].Y - RoofProfile_1[0].Y) / (RoofProfile_1[1].X - RoofProfile_1[0].X));
-            }
-            else
-            {
-                B = Math.Abs(roof_profile_2[0].X - roof_profile_1[0].X);
-                L = Math.Max(Math.Abs(roof_profile_1[count_1 - 1].Z - roof_profile_1[0].Z), Math.Abs(roof_profile_2[count_2 - 1].Z - roof_profile_2[0].Z));
+            //// Vectors for the points of the east-west frame 1 structure profile based on the provided dimensions
+            //// 0,0 is assumed to be lower left for windward wall
+            //WW_GRD_1 = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+            //WW_15_1 = new Vector4(0.0f, 15.0f, 0.0f, 1.0f);
+            //WW_H_1 = new Vector4(roof_profile_1[0].X, roof_profile_1[0].Y, roof_profile_1[0].Z, 1.0f);
+            //LW_H_1 = new Vector4(roof_profile_1[count_1 - 1].X, roof_profile_1[count_1 - 1].Y, roof_profile_1[count_1 - 1].Z, 1.0f);
+            //LW_GRD_1 = new Vector4(roof_profile_1[count_1 - 1].X, 0.0f, roof_profile_1[count_1 - 1].Z, 1.0f);
+            //LW_15_1 = new Vector4(roof_profile_1[count_1 - 1].X, 15.0f, roof_profile_1[count_1 - 1].Z, 1.0f);
 
-                // Parallel to ridge should have 0 slope.
-                RoofSlope = 0.0;
-            }
+            //// Set the origin of the model to be the midpoint at ground level -- uses the average of the base points
+            //ORIGIN_1 = new Vector4((float)(0.5 * (WW_GRD_1.X + LW_GRD_1.X) + 0.5 * ((float)WW_GRD_2.X + (float)LW_GRD_2.X) / 2.0f),
+            //    (float)(0.5 * (WW_GRD_1.Y + LW_GRD_1.Y) + 0.5 * ((float)WW_GRD_2.Y + (float)LW_GRD_2.Y) / 2.0f),
+            //    (float)(0.5 * (WW_GRD_1.Z + LW_GRD_1.Z) + 0.5 * ((float)WW_GRD_2.Z + (float)LW_GRD_2.Z) / 2.0f),
+            //    1.0f);
 
-            // Vectors for the points of the east-west frame 1 structure profile based on the provided dimensions
-            // 0,0 is assumed to be lower left for windward wall
-            WW_GRD_1 = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
-            WW_15_1 = new Vector4(0.0f, 15.0f, 0.0f, 1.0f);
-            WW_H_1 = new Vector4(roof_profile_1[0].X, roof_profile_1[0].Y, roof_profile_1[0].Z, 1.0f);
-            LW_H_1 = new Vector4(roof_profile_1[count_1 - 1].X, roof_profile_1[count_1 - 1].Y, roof_profile_1[count_1 - 1].Z, 1.0f);
-            LW_GRD_1 = new Vector4(roof_profile_1[count_1 - 1].X, 0.0f, roof_profile_1[count_1 - 1].Z, 1.0f);
-            LW_15_1 = new Vector4(roof_profile_1[count_1 - 1].X, 15.0f, roof_profile_1[count_1 - 1].Z, 1.0f);
+            //// Vectors for the points of the east-west frame 2 structure profile based on the provided dimensions
+            //// 0,0 is assumed to be lower left for windward wall
+            //WW_GRD_2 = new Vector4(roof_profile_2[0].X, 0.0f, roof_profile_2[0].Z, 1.0f);
+            //WW_15_2 = new Vector4(roof_profile_2[0].X, 15.0f, roof_profile_2[0].Z, 1.0f);
+            //WW_H_2 = new Vector4(roof_profile_2[0].X, roof_profile_2[0].Y, roof_profile_2[0].Z, 1.0f);
+            //LW_H_2 = new Vector4(roof_profile_2[count_2 - 1].X, roof_profile_2[count_2 - 1].Y, roof_profile_2[count_2 - 1].Z, 1.0f);
+            //LW_GRD_2 = new Vector4(roof_profile_2[count_2 - 1].X, 0.0f, roof_profile_2[count_2 - 1].Z, 1.0f);
+            //LW_15_2 = new Vector4(roof_profile_2[count_2 - 1].X, 15.0f, roof_profile_2[count_2 - 1].Z, 1.0f);
 
-            // Set the origin of the model to be the midpoint at ground level -- uses the average of the base points
-            ORIGIN_1 = new Vector4((float)(0.5 * (WW_GRD_1.X + LW_GRD_1.X) + 0.5 * ((float)WW_GRD_2.X + (float)LW_GRD_2.X) / 2.0f),
-                (float)(0.5 * (WW_GRD_1.Y + LW_GRD_1.Y) + 0.5 * ((float)WW_GRD_2.Y + (float)LW_GRD_2.Y) / 2.0f),
-                (float)(0.5 * (WW_GRD_1.Z + LW_GRD_1.Z) + 0.5 * ((float)WW_GRD_2.Z + (float)LW_GRD_2.Z) / 2.0f),
-                1.0f);
+            //// Set the origin of the model to be the midpoint at ground level -- uses the average of the base points
+            //ORIGIN_2 = new Vector4((float)(0.5 * (WW_GRD_1.X + LW_GRD_1.X) + 0.5 * ((float)WW_GRD_2.X + (float)LW_GRD_2.X) / 2.0f),
+            //    (float)(0.5 * (WW_GRD_1.Y + LW_GRD_1.Y) + 0.5 * ((float)WW_GRD_2.Y + (float)LW_GRD_2.Y) / 2.0f),
+            //    (float)(0.5 * (WW_GRD_1.Z + LW_GRD_1.Z) + 0.5 * ((float)WW_GRD_2.Z + (float)LW_GRD_2.Z) / 2.0f),
+            //    1.0f);
 
-            // Vectors for the points of the east-west frame 2 structure profile based on the provided dimensions
-            // 0,0 is assumed to be lower left for windward wall
-            WW_GRD_2 = new Vector4(roof_profile_2[0].X, 0.0f, roof_profile_2[0].Z, 1.0f);
-            WW_15_2 = new Vector4(roof_profile_2[0].X, 15.0f, roof_profile_2[0].Z, 1.0f);
-            WW_H_2 = new Vector4(roof_profile_2[0].X, roof_profile_2[0].Y, roof_profile_2[0].Z, 1.0f);
-            LW_H_2 = new Vector4(roof_profile_2[count_2 - 1].X, roof_profile_2[count_2 - 1].Y, roof_profile_2[count_2 - 1].Z, 1.0f);
-            LW_GRD_2 = new Vector4(roof_profile_2[count_2 - 1].X, 0.0f, roof_profile_2[count_2 - 1].Z, 1.0f);
-            LW_15_2 = new Vector4(roof_profile_2[count_2 - 1].X, 15.0f, roof_profile_2[count_2 - 1].Z, 1.0f);
+            //double max_ht = ComputeMeanRoofHeight();
+            //// Take the average of the lesser of the windward wall height and the leeward wall height -- needed in case of unequal wall heights.
+            //H = 0.5 * (max_ht + Math.Min(RoofProfile_1[0].Y, RoofProfile_1[RoofProfile_1.Length - 1].Y));
 
-            // Set the origin of the model to be the midpoint at ground level -- uses the average of the base points
-            ORIGIN_2 = new Vector4((float)(0.5 * (WW_GRD_1.X + LW_GRD_1.X) + 0.5 * ((float)WW_GRD_2.X + (float)LW_GRD_2.X) / 2.0f),
-                (float)(0.5 * (WW_GRD_1.Y + LW_GRD_1.Y) + 0.5 * ((float)WW_GRD_2.Y + (float)LW_GRD_2.Y) / 2.0f),
-                (float)(0.5 * (WW_GRD_1.Z + LW_GRD_1.Z) + 0.5 * ((float)WW_GRD_2.Z + (float)LW_GRD_2.Z) / 2.0f),
-                1.0f);
-
-            double max_ht = ComputeMeanRoofHeight();
-            // Take the average of the lesser of the windward wall height and the leeward wall height -- needed in case of unequal wall heights.
-            H = 0.5 * (max_ht + Math.Min(RoofProfile_1[0].Y, RoofProfile_1[RoofProfile_1.Length - 1].Y));
-
-            // Must be after the H declaration -- define the H/2, H, 2H, >2H locatons
-            GetRoofPressureZonePoints();
+            //// Must be after the H declaration -- define the H/2, H, 2H, >2H locatons
+            //GetRoofPressureZonePoints();
         }
 
-        private double ComputeMeanRoofHeight()
-        {
-            // Determine our mean roof height -- using the y-coords (1, 3, 5, 7, ...) of roof_profile
-            // and determine if the roof is flat or not.
-            double max_ht = 0;
-            double wall_ht = RoofProfile_1[0].Y;
-            foreach (var item in RoofProfile_1)
-            {
-                if (item.Y > max_ht)
-                {
-                    max_ht = item.Y;
-                }
-            }
+        //private double ComputeMeanRoofHeight()
+        //{
+        //    // Determine our mean roof height -- using the y-coords (1, 3, 5, 7, ...) of roof_profile
+        //    // and determine if the roof is flat or not.
+        //    double max_ht = 0;
+        //    double wall_ht = RoofProfile_1[0].Y;
+        //    foreach (var item in RoofProfile_1)
+        //    {
+        //        if (item.Y > max_ht)
+        //        {
+        //            max_ht = item.Y;
+        //        }
+        //    }
 
-            return max_ht;
-        }
+        //    return max_ht;
+        //}
 
-        public virtual void GetSlopedRoofPressureZonePoints() { throw new InvalidOperationException("A flat roof should not have sloped pressure points"); }
-        public virtual void GetRoofPressureZonePoints() { GetFlatRoofPressureZonePoints(); }
+        //public virtual void GetSlopedRoofPressureZonePoints() { throw new InvalidOperationException("A flat roof should not have sloped pressure points"); }
+        //public virtual void GetRoofPressureZonePoints() { GetFlatRoofPressureZonePoints(); }
 
-        /// <summary>
-        /// Get the zone boundary locations for the roof measure relative to a reference point (usually 0,0)
-        /// </summary>
-        /// <returns></returns>
-        public virtual void GetFlatRoofPressureZonePoints()
-        { 
-            if (L < H * 0.5)
-            {
-                RoofZonePts_1 = new Vector4[] {
-                WW_H_1,
-                LW_H_1};
-                RoofZonePts_2 = new Vector4[] {
-                WW_H_2,
-                LW_H_2};
-            }
-            else if (L < H)
-            {
-                RoofZonePts_1 = new Vector4[] {
-                    WW_H_1,
-                    new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    LW_H_1};
-                RoofZonePts_2 = new Vector4[] {
-                    WW_H_2,
-                    new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    LW_H_2};
-            }
-            else if (L < H * 2.0)
-            {
-                RoofZonePts_1 = new Vector4[] {
-                    WW_H_1,
-                    new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    LW_H_1};
-                RoofZonePts_2 = new Vector4[] {
-                    WW_H_2,
-                    new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    new Vector4((float)(WW_H_2.X + H), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    new Vector4((float)(WW_H_2.X + H), WW_H_2.Y, WW_H_1.Z, 1.0f),
-                    LW_H_2};
-            }
-            else
-            {
-                RoofZonePts_1 = new Vector4[] {
-                    WW_H_1,
-                    new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    new Vector4((float)(WW_H_1.X + H * 2.0), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    new Vector4((float)(WW_H_1.X + H * 2.0), WW_H_1.Y, WW_H_1.Z, 1.0f),
-                    LW_H_1};
-                RoofZonePts_2 = new Vector4[] {
-                    WW_H_2,
-                    new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    new Vector4((float)(WW_H_2.X + H), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    new Vector4((float)(WW_H_2.X + H), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    new Vector4((float)(WW_H_2.X + H * 2.0), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    new Vector4((float)(WW_H_2.X + H * 2.0), WW_H_2.Y, WW_H_2.Z, 1.0f),
-                    LW_H_2};
-            }
-        }
+        ///// <summary>
+        ///// Get the zone boundary locations for the roof measure relative to a reference point (usually 0,0)
+        ///// </summary>
+        ///// <returns></returns>
+        //public virtual void GetFlatRoofPressureZonePoints()
+        //{ 
+        //    if (L < H * 0.5)
+        //    {
+        //        RoofZonePts_1 = new Vector4[] {
+        //        WW_H_1,
+        //        LW_H_1};
+        //        RoofZonePts_2 = new Vector4[] {
+        //        WW_H_2,
+        //        LW_H_2};
+        //    }
+        //    else if (L < H)
+        //    {
+        //        RoofZonePts_1 = new Vector4[] {
+        //            WW_H_1,
+        //            new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            LW_H_1};
+        //        RoofZonePts_2 = new Vector4[] {
+        //            WW_H_2,
+        //            new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            LW_H_2};
+        //    }
+        //    else if (L < H * 2.0)
+        //    {
+        //        RoofZonePts_1 = new Vector4[] {
+        //            WW_H_1,
+        //            new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            LW_H_1};
+        //        RoofZonePts_2 = new Vector4[] {
+        //            WW_H_2,
+        //            new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            new Vector4((float)(WW_H_2.X + H), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            new Vector4((float)(WW_H_2.X + H), WW_H_2.Y, WW_H_1.Z, 1.0f),
+        //            LW_H_2};
+        //    }
+        //    else
+        //    {
+        //        RoofZonePts_1 = new Vector4[] {
+        //            WW_H_1,
+        //            new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            new Vector4((float)(WW_H_1.X + H * 0.5), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            new Vector4((float)(WW_H_1.X + H), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            new Vector4((float)(WW_H_1.X + H * 2.0), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            new Vector4((float)(WW_H_1.X + H * 2.0), WW_H_1.Y, WW_H_1.Z, 1.0f),
+        //            LW_H_1};
+        //        RoofZonePts_2 = new Vector4[] {
+        //            WW_H_2,
+        //            new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            new Vector4((float)(WW_H_2.X + H * 0.5), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            new Vector4((float)(WW_H_2.X + H), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            new Vector4((float)(WW_H_2.X + H), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            new Vector4((float)(WW_H_2.X + H * 2.0), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            new Vector4((float)(WW_H_2.X + H * 2.0), WW_H_2.Y, WW_H_2.Z, 1.0f),
+        //            LW_H_2};
+        //    }
+        //}
     }
 }
